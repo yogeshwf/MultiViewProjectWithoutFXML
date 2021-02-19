899c3ead2087f5abbe08ab0bc92e0e5276669fe7
yogeshwf <yogesh.webforest@gmail.com>
2021-02-19 16:20:45 +0800

    Add project Files

---
+++ src/main/java/com/gluonapplication/GluonApplication.java
@@ -0,0 +1,35 @@
+package com.gluonapplication;
+
+import com.gluonapplication.views.PrimaryView;
+import com.gluonapplication.views.SecondaryView;
+import com.gluonhq.charm.glisten.application.MobileApplication;
+import com.gluonhq.charm.glisten.visual.Swatch;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class GluonApplication extends MobileApplication {
+
+    public static final String PRIMARY_VIEW = HOME_VIEW;
+    public static final String SECONDARY_VIEW = "Secondary View";
+    
+    @Override
+    public void init() {
+        addViewFactory(PRIMARY_VIEW, PrimaryView::new);
+        addViewFactory(SECONDARY_VIEW, SecondaryView::new);
+        
+        DrawerManager.buildDrawer(this);
+    }
+
+    @Override
+    public void postInit(Scene scene) {
+        Swatch.BLUE.assignTo(scene);
+
+        scene.getStylesheets().add(GluonApplication.class.getResource("style.css").toExternalForm());
+        ((Stage) scene.getWindow()).getIcons().add(new Image(GluonApplication.class.getResourceAsStream("/icon.png")));
+    }
+
+    public static void main(String args[]) {
+        launch(args);
+    }
+}
